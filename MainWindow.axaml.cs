12e9f55f25f40514de4784cb2f2d01970ca9fedc
Gerald Stevens <gerbob@gmail.com>
2023-04-26 20:48:21 -0600

    Adding Close Handler

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -7,6 +7,9 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-        DataContext = new MainWindowViewModel();
+
+        var closeHandler = () => Close();
+
+        DataContext = new MainWindowViewModel(closeHandler);
     }
 }
--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -1,29 +1,51 @@
 
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System;
 using System.ComponentModel;
 
 namespace Avapad;
 public class MainWindowViewModel : ObservableRecipient
 {
     private string? _fileContents;
-    public bool HasChanges {get;set;}
+    private int _fontSize = 12;
+    private readonly Action _closeHandler;
 
-    public string FileName {get;private set;} = "test_file.txt";
+    public bool HasChanges { get; set; }
 
-    public string? FileContents {get => _fileContents; set => SetProperty(ref _fileContents, value);}
+    public string FileName { get; private set; } = "test_file.txt";
+
+    public string? FileContents { get => _fileContents; set => SetProperty(ref _fileContents, value); }
+
+    public int FontSize { get => _fontSize; set => SetProperty(ref _fontSize, value); }
 
     public RelayCommand SaveCommand {get; }
     public RelayCommand SaveAsCommand {get;}
     public RelayCommand OpenCommand {get;}
     public RelayCommand NewCommand {get;}
 
-    public MainWindowViewModel()
+    public RelayCommand HelpCommand {get;}
+
+    public RelayCommand AboutCommand {get;}
+
+    public RelayCommand CloseCommand {get;}
+
+    public MainWindowViewModel(Action closeHandler)
     {
+        _closeHandler = closeHandler;
+
         SaveCommand = new RelayCommand(() => Save(), () => HasChanges);
         SaveAsCommand = new RelayCommand(() => SaveAs());
         OpenCommand = new RelayCommand(() => Open());
         NewCommand = new RelayCommand(() => New());
+        HelpCommand = new RelayCommand(() => ShowHelp());
+        AboutCommand = new RelayCommand(() => ShowAbout());
+        CloseCommand = new RelayCommand(() => CloseApplication());
+    }
+
+    private void CloseApplication()
+    {
+        throw new NotImplementedException();
     }
 
     private void Save()
@@ -36,4 +58,8 @@ public class MainWindowViewModel : ObservableRecipient
     private void Open() {}
 
     private void New() {}
+
+    private void ShowHelp() {}
+
+    private void ShowAbout() {}
 }
